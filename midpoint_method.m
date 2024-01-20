e9c132a5f65680d2329d4dbc660db32437a0b7d1
MD ARIF HOSSAIN <arif.hossain.shakib@gmail.com>
2024-01-21 01:42:33 +0600

    adding midpoint_method

--- midpoint_method.m
+++ midpoint_method.m
@@ -5,11 +5,11 @@ f=@(x,y) (y-x^2+1);
 x=x0:h:xn;
 y(1)=0.5;
 for n= 1:length(x)-1
- yp(n+1) = y(n) + h*f(x(n),y(n));
- y(n+1) = y(n)+ (h/2)*(f(x(n),y(n)) +
-f(x(n+1),yp(n+1)));
+ a = f(x(n),y(n));
+ b = f(x(n)+h/2,y(n)+h/2*a);
+ y(n+1) = y(n) + h*b;
 end
 Exact = x.^2+2*x-(exp(x)/2)+1;
 absError = abs(Exact-y);
-fprintf('\t\txi\t\tExact Value\t\tE''sModified\t\tabsError\n')
+fprintf('\t\txi\t\tExact Value\t\tMid PointM\t\tabsError\n')
 fprintf('\t\t%.1f\t\t%.8f\t\t%.8f\t\t%.8f\n',[x',Exact',y',absError']')
